af4c752bd49f848a51561f1dbdb5bd44f877607b
Nick Mattei <nmattei89@gmail.com>
2015-09-09 12:06:24 -0600

    some basic crud up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,25 @@
 class ProductsController < ApplicationController
   def index
-
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+  end
 
+
+  def create
+    @product = Product.new(post_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+
+  def post_params
+    params.require(:product).permit(:name, :brand)
   end
 
 
